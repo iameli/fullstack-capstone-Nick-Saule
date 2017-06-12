49531da58af00e5803e49c0dc44eb91df5a58d15
nfjones87 <nfjones87@gmail.com>
2017-06-12 13:40:39 -0700

    test

--- test/test.js
+++ test/test.js
@@ -8,12 +8,17 @@ chai.use(chaiHttp);
 
 const {app} = require('../server');
 
+
 describe('access root', function() {
     it('should return 200 and html', function() {
       return chai.request(app)
         .get('/')
         .then(function(res) {
           res.should.have.status(200);
+          res.should.be.html;
         });
     });
 });
+
+
+
