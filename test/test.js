182ba03679af7f4685121c232ef90c105154d154
nfjones87 <nfjones87@gmail.com>
2017-06-12 12:33:09 -0700

    first test working

--- test/test.js
+++ test/test.js
@@ -1,5 +1,19 @@
 'use strict';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const faker = require('faker');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+const {app} = require('../server');
+
+describe('access root', function() {
+    it('should return 200 and html', function() {
+      return chai.request(app)
+        .get('/')
+        .then(function(res) {
+          res.should.have.status(200);
+        });
+    });
+});
